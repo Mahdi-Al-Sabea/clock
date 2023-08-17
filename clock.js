168892cc0410872bd65e2c713e0b2d748313a4d2
Mahdi-Al-Sabea <mahdisabeh12345@gmail.com>
2023-08-17 22:05:01 +0300

    finalising project
    done

--- clock.js
+++ clock.js
@@ -4,18 +4,23 @@
 function showTime(){
     
     const currentDate=new Date();
+    currentDate.setMonth(8);
+
     
     let hours=document.querySelector('.hours');
     let minutes=document.querySelector('.minutes');
     let seconds=document.querySelector('.seconds');
     let timePeriod=document.querySelector('.time-period');
+    let date=document.querySelector('.date');
     
     hours.innerText=currentDate.getHours();
     minutes.innerText=currentDate.getMinutes();
     seconds.innerText=currentDate.getSeconds();
     timePeriod.innerText=getTimeSpan(currentDate.getHours());
 
-    console.log(currentDate.getMonth);
+    date.innerHTML=`${currentDate.getMonth()}/${currentDate.getUTCDate()}/${currentDate.getFullYear()}`;
+
+   
     
     
     
